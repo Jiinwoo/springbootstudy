d02b0143cfd37c8a26c72005525936480d37938a
jinwoojung <jwjung5038@gmail.com>
2020-01-05 16:51:12 +0900

    테스트환경이 Random port 일때 + Service를 Mock으로 사용할 때

--- src/test/java/me/jung/sample/SampleControllerTest.java
+++ src/test/java/me/jung/sample/SampleControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,8 +17,14 @@ public class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception{
+        when(mockSampleService.getName()).thenReturn("jinwoo");
+
+
         String result = testRestTemplate.getForObject("/hello",String.class);
         assertThat(result).isEqualTo("hellojinwoo");
     }
