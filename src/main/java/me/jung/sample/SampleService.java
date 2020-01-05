b91e5816110678ae65bd2a91bb9a2c91ee5da0cc
jinwoojung <jwjung5038@gmail.com>
2020-01-05 16:42:57 +0900

    테스트환경이 MOCK일때

---
+++ src/main/java/me/jung/sample/SampleService.java
@@ -0,0 +1,10 @@
+package me.jung.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+    public String getName() {
+        return "jinwoo";
+    }
+}
