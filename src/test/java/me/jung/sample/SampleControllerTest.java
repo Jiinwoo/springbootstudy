073003c36add7fdc6ec359db8797a225681ddc46
jinwoojung <jwjung5038@gmail.com>
2020-01-05 17:08:57 +0900

    슬라이싱 테스트 일부만 빈으로 등록해서 테스트

--- src/test/java/me/jung/sample/SampleControllerTest.java
+++ src/test/java/me/jung/sample/SampleControllerTest.java
@@ -3,29 +3,31 @@ package me.jung.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
     //비동기로 처리
-    @Autowired
-    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     public void hello() throws Exception{
         when(mockSampleService.getName()).thenReturn("jinwoo");
 
-        webTestClient.get().uri("/hello").exchange().expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("hellojinwoo");
-
+       mockMvc.perform(get("/hello"))
+               .andExpect(content().string("hellojinwoo"));
     }
 }
