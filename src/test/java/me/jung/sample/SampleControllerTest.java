0aad43acb52fb31f1d5046a8a68f9ed641c6a6fa
jinwoojung <jwjung5038@gmail.com>
2020-01-05 17:16:32 +0900

    로그 캡쳐

--- src/test/java/me/jung/sample/SampleControllerTest.java
+++ src/test/java/me/jung/sample/SampleControllerTest.java
@@ -1,13 +1,16 @@
 package me.jung.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,6 +19,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
     //비동기로 처리
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+    //로그에찍히는것을 확인할수있도록
 
     @MockBean
     SampleService mockSampleService;
@@ -29,5 +36,8 @@ public class SampleControllerTest {
 
        mockMvc.perform(get("/hello"))
                .andExpect(content().string("hellojinwoo"));
+       assertThat(outputCapture.toString())
+               .contains("holoman")
+               .contains("skip");
     }
 }
