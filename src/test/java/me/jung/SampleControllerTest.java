4ff076829cd580ec085c7fccf57736d81bb4bed9
jinwoojung <jwjung5038@gmail.com>
2020-01-10 23:37:49 +0900

    MVC 8 HtmlUnit 테스트

--- src/test/java/me/jung/SampleControllerTest.java
+++ src/test/java/me/jung/SampleControllerTest.java
@@ -1,37 +1,27 @@
 package me.jung;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.core.StringContains.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
     @Autowired
-    MockMvc mockMvc;
+    WebClient webClient;
 
     @Test
     public void hello() throws Exception {
-        /**
-         * 요청 "/"
-         * 으답
-         * -모델 name : jinwoo
-         * - 뷰 이름 : jinwoo
-         */
+      HtmlPage page = webClient.getPage("/hello");
+      HtmlHeading1 h1 =  page.getFirstByXPath("//h1");
+      assertThat(h1.getTextContent()).isEqualToIgnoringCase("jinwoo");
 
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(view().name("hello"))
-                .andExpect(model().attribute("name","jinwoo"))
-                .andExpect(content().string(containsString("jinwoo")));
     }
 }
