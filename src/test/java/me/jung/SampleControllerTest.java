4c4fd112517a55040eb35fa1c48293eee00da913
jinwoojung <jwjung5038@gmail.com>
2020-01-10 23:27:43 +0900

    MVC 7 타임리프

---
+++ src/test/java/me/jung/SampleControllerTest.java
@@ -0,0 +1,37 @@
+package me.jung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        /**
+         * 요청 "/"
+         * 으답
+         * -모델 name : jinwoo
+         * - 뷰 이름 : jinwoo
+         */
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name","jinwoo"))
+                .andExpect(content().string(containsString("jinwoo")));
+    }
+}
