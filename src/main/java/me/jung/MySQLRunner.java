8a63400f8c224918f4bc19186b917d51040de1cd
jinwoojung <jwjung5038@gmail.com>
2020-01-11 19:20:24 +0900

    스프링 데이터 MYSQL

---
+++ src/main/java/me/jung/MySQLRunner.java
@@ -0,0 +1,38 @@
+package me.jung;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class MySQLRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()){
+            System.out.println(dataSource.getClass());
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            // table 생성
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS user");
+            String sql = "CREATE TABLE USER (id INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+            statement.executeUpdate(sql);
+
+        }
+        jdbcTemplate.execute("INSERT INTO USER VALUES(1,'jinwoo')");
+    }
+}
